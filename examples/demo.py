89f4cc5f6e73e8ea00c60e6bf8814cf588c50c9b
Yangming Huang <yangming@inceptioglobal.ai>
2020-06-12 22:44:50 +0800

    fix ci/cd and some cleanup to the demo

--- examples/demo.py
+++ examples/demo.py
@@ -1,43 +1,26 @@
 #!/usr/bin/env python
-import json
 
 import click
 
 from examples.util import interval
 from kebab import default_source, kebab_config, Field
 
-source = default_source()
-
 
 @kebab_config(auto_repr=True)
 class DemoConfig:
-    str_value = Field("ready", default_value=5)
+    str_value = Field("ready", default_value="5", expected_type=str)
     int_value = Field("int", required=True, expected_type=int)
 
 
-@click.group()
-def cli():
-    pass
-
-
-@cli.command()
-@click.option('-i', '--interval', "by_secs", default=-1, type=float)
+@click.command()
+@click.option('-w', '--watch', "by_secs", default=-1, type=float)
 def cast(by_secs):
     while True:
         with interval(by_secs):
-            value = source.cast("demo", DemoConfig)
+            value = default_source().cast("demo", DemoConfig)
             click.echo(value)
 
 
-@cli.command()
-@click.option('-i', '--interval', "by_secs", default=-1, type=float)
-@click.argument('key', default='.', nargs=1, type=str)
-def get(by_secs, key):
-    while True:
-        with interval(by_secs):
-            value = source.get(key)
-            click.echo(json.dumps(value))
-
-
 if __name__ == '__main__':
-    cli()
+    # please reference to kebab cli for single get type of command
+    cast()
